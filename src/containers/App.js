3e8648dbf036c37945da68e17890244d1056253d
Olawale <stwalez1@gmail.com>
2019-03-24 19:51:26 +0100

    utilized redux architecture

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 //import { robots } from '../robots';
@@ -6,16 +7,35 @@ import ErrorBoundary from '../components/ErrorBoundry';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+//mapStateToProps === tell me what piece of state to listen to so i can send it out as props
+const mapStateToProps = state =>{
+	return {
+		searchField: state.searchField
+	}
+}
+
+//mapDispatchToProps === tell me what props to listen thAT are actions that needs to get dispatched
+const mapDispatchToProps = (dispatch) =>{
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+
+}
+//dispatch triggers/sends the actions
+
 class App extends Component{
 	constructor() {
 		super()
 		this.state = {
 			robots : [],
-			searchfield : ''
+			//searchfield : ''
 		}
 	}
 
 componentDidMount(){
+	//console.log(this.props.store.getState());
 	fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response=> response.json())
 	 		.then(users=> this.setState({ robots: users}));
@@ -23,21 +43,22 @@ componentDidMount(){
 	//this.setState({ robots: robots});
 	
 }
-	onSearchChange = (event) => {
+	/*onSearchChange = (event) => {
 		this.setState({searchfield : event.target.value})
-	}
+	}*/
 	
 	render(){
-		const { robots, searchfield} = this.state
+		const { robots, /*searchfield*/} = this.state
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 		return !robots.length ? 
 			<h1>Loading</h1> :
 			(
 				<div className = "tc">
 					<h1 className="f1">RoboFriends</h1>
-					<SearchBox searchChange ={this.onSearchChange}/>
+					<SearchBox searchChange ={onSearchChange}/>
 					<Scroll>
 						<ErrorBoundary>
 							<CardList robots = {filteredRobots}/>
@@ -47,4 +68,4 @@ componentDidMount(){
 			);
 	}
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import {createLogger } from 'redux-logger'
 import './index.css';
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 //import {robots} from './robots'
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
+
 ReactDOM.render(
-			<App/>, document.getElementById('root'));
+			<Provider store={store}>
+				<App />
+			</Provider>,
+			 document.getElementById('root'));
+
+//Provider component takes care of passing down the store to the App components
 //// If you want your app to work offline and load faster, you can change
 
 // unregister() to register() below. Note this comes with some pitfalls.
